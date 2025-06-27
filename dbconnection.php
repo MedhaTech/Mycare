a34cc3f68b1038aee4d508ab6eb255270eeb45c6
tech-girish <girish.extravis@gmail.com>
2025-06-27 13:47:01 +0530

    debug

--- dbconnection.php
+++ dbconnection.php
@@ -1,9 +1,12 @@
 <?php
+error_reporting(E_ALL); 
+ini_set('display_errors', '1'); 
+ini_set('display_startup_errors', '1'); 
 /*
 $servername = "localhost";
 $username = "root"; 
 $password = "";     
-$database = "medical";
+$database = "my_care";
 
 
 $conn = new mysqli($servername, $username, $password, $database);
