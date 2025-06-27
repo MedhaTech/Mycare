f49192ae235f0ab7a069549ddca33983d4769638
Lalith-cmd <lalithchandra.r@gmail.com>
2025-06-27 13:18:51 +0530

    Update doctors-list.php

--- doctors-list.php
+++ doctors-list.php
@@ -14,6 +14,11 @@
                         </div> 
 
                             <?php
+                            
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+include 'header.php';
+
                             include 'dbconnection.php';
 
                             
