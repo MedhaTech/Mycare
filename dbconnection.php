c116bc2a032216f84a66348629a9b1a6c8b100c5
geetha-medha <geethanjali@medhatech.in>
2025-07-07 09:56:12 +0530

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $servername = "localhost";
 $username = "root"; 
 $password = "";     
@@ -12,8 +12,8 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-/*
 
+*/
 $servername = "192.185.129.71";
 $username = "medha_mycare"; 
 $password = "peO*aDq0=Hb&";     
@@ -26,5 +26,5 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);    
 }
-*/
+
 ?>
